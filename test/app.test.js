eb98d583eb42838e35286866726d8923e5163233
Felipe Gutiérrez <caracoloro@gmail.com>
2025-06-20 18:22:02 -0400

    fix: update from commonjs to module

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,13 @@
-const request = require("supertest");
-const app = require("../src/app");
+import request from "supertest";
+import { app } from "../src/app";
 
 describe("GET /api/inmuebles", () => {
-	it("debería retornar una lista de inmuebles", async () => {
-		const res = await request(app).get("/api/inmuebles");
+  it("debería retornar una lista de inmuebles", async () => {
+    const res = await request(app).get("/api/inmuebles");
 
-		expect(res.statusCode).toBe(200);
-		expect(res.body.length).toBeGreaterThan(0);
-		expect(res.body[0]).toHaveProperty("direccion");
-		expect(res.body[0]).toHaveProperty("precio");
-	});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0]).toHaveProperty("direccion");
+    expect(res.body[0]).toHaveProperty("precio");
+  });
 });
