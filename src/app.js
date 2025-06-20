15abca01f1ea7dd9f7a8f65dc8eaa19fc490722f
Felipe Gutiérrez <caracoloro@gmail.com>
2025-06-20 18:17:44 -0400

    feat: check prettierd

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const inmuebles = [
-	{ id: 1, direccion: "Calle 123", precio: 150000, tipo: "Apartamento" },
-	{ id: 2, direccion: "Avenida 456", precio: 230000, tipo: "Casa" },
-	{ id: 3, direccion: "Carrera 789", precio: 120000, tipo: "Estudio" },
+  { id: 1, direccion: "Cae 123", precio: 150000, tipo: "Apartamento" },
+  { id: 2, direccion: "Avenida 456", precio: 230000, tipo: "Casa" },
+  { id: 3, direccion: "Carrera 789", precio: 120000, tipo: "Estudio" },
 ];
 
-app.get("/api/inmuebles", (req, res) => {
-	res.json(inmuebles);
+app.get("/api/inmuebles", (_req, res) => {
+  res.json(inmuebles);
 });
 
-module.exports = app;
+export default app;
